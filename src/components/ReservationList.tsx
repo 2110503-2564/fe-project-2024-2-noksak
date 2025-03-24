a04acb11d1e25346aba9733bf583074402f95d2d
Pisit Kanchanapinpong <pisit.eicq@gmail.com>
2025-03-24 13:36:14 +0700

    Edit reservationlist

--- src/components/ReservationList.tsx
+++ src/components/ReservationList.tsx
@@ -3,18 +3,23 @@ import { ReservationItem, ReservationJson } from '../../interface'
 
 const ReservationList = async ({reservationJson}: {reservationJson: Promise<ReservationJson>}) => {
 
-    
+    const reservationReady = await reservationJson;
 
-  return (
-    <section className="m-1 flex flex-wrap gap-2">
-        {(await (reservationJson)).data.map((reservation: ReservationItem) => (
-        <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-2 text-black" key={reservation._id}>
-        <div className="text-xl">{reservation.restaurant.name}</div>
-        <div className="text-sm">Reservation Date {reservation.apptDate}</div>            
-        </div>
-        ))}
-      </section>
-  )
+    return reservationReady.count===0? (
+        <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 font-ubuntu text-lg text-center flex flex-col items-center gap-4">
+          <span className="text-6xl">📖❌</span> {/* ใช้ Emoji แทน BookX */}
+          No Reservation
+        </span>
+      ):(
+        <section className="m-1 flex flex-wrap gap-2">
+            {reservationReady.data.map((reservation: ReservationItem) => (
+            <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-2 text-black" key={reservation._id}>
+            <div className="text-xl">{reservation.restaurant.name}</div>
+            <div className="text-sm">Reservation Date {reservation.apptDate}</div>            
+            </div>
+            ))}
+          </section>
+      )
 }
 
 export default ReservationList
